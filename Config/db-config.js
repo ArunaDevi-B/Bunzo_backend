c007801fd1b90f3f3db963c2603a6b3eefe719ed
ArunaDevi-B <b.arunadevi30@gmail.com>
2022-10-23 19:56:41 +0530

    db moving to atlas

--- Config/db-config.js
+++ Config/db-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect('mongodb://localhost:27017/order-management', {
+    mongoose.connect('mongodb+srv://Aruna:Aruna123@cluster0.ndwq8ot.mongodb.net/order_management', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
